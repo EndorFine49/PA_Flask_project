9ca4a681ea389f7e853ffd37fee6ce17d3c8f99c
EndorFine <anton.work49@gmail.com>
2024-03-21 22:56:52 +0000

    First steps

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,15 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
+
 @app.route('/')
-def hello_world():
-    return 'Something big will be here one day'
+def index():
+    return render_template("main.html")
 
 @app.route('/wibble')
 def wibble():
