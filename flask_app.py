2bc13d9333072b10133300cc6666115363df1a4a
EndorFine <anton.work49@gmail.com>
2024-04-09 13:13:30 +0300

    db password change

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,7 @@ app.config["DEBUG"] = True
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="EndorFine",
-    password="6RxLryxrPY#4yB@",
+    password="6RxLryxrPY#4yB",
     hostname="EndorFine.mysql.pythonanywhere-services.com",
     databasename="EndorFine$comments",
 )
@@ -16,6 +16,12 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+class Comment(db.Model):
+    __tablename__="comments"
+
+    id=db.Column(db.integer, primary_key = True)
+    content=db.Column(db.string(4096))
 
 comments = []
 
